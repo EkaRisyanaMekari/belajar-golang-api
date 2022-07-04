27368428e2c820993423903ce4c02d5cc7f50bef
Eka Risyana <eka.risyana@mekari.com>
2022-07-04 15:00:59 +0700

    handle query and param

--- main.go
+++ main.go
@@ -9,19 +9,40 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"name": "Eka",
-			"bio":  "Software Developer",
-		})
+	router.GET("/", rootHandler)
+	router.GET("/hello", helloHandler)
+	router.GET("/books/:id", booksHandler)
+	router.GET("/book-detail", bookDetailHandler)
+	router.Run()
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Eka",
+		"bio":  "Software Developer",
 	})
 
-	router.GET("/hello", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"content":  "judul",
-			"subtitle": "subjudul",
-		})
-	})
+}
 
-	router.Run(":8888")
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"content":  "judul",
+		"subtitle": "subjudul",
+	})
+}
+
+func booksHandler(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{
+		"id":       id,
+		"content":  "judul",
+		"subtitle": "subjudul",
+	})
+}
+
+func bookDetailHandler(c *gin.Context) {
+	title := c.Query("title")
+	c.JSON(http.StatusOK, gin.H{
+		"title": title,
+	})
 }
