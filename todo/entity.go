fb0462eec8c71f8f452e881ab335f79d99935728
Eka Risyana <eka.risyana@mekari.com>
2023-01-24 06:24:48 +0700

    todo status is not required

--- todo/entity.go
+++ todo/entity.go
@@ -5,7 +5,7 @@ import "time"
 type Todo struct {
 	ID          int    `json:"id"`
 	Description string `json:"description" binding:"required"`
-	Status      bool   `json:"status" binding:"required"`
+	Status      bool   `json:"status"`
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
 }
